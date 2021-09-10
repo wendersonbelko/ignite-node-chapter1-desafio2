5e6cd939983889faa4ccb4d34ab7622e2f7029d2
belko <wbelko@outlook.com>
2021-09-10 00:30:28 -0300

    finish todos

--- src/index.js
+++ src/index.js
@@ -10,19 +10,73 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'username not fount' })
+  }
+
+  request.user = user;
+
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  const isPro = user.pro === true;
+  const maxTodo = user.todos.length === 10;
+
+  if (isPro === false && maxTodo === true) {
+    return response.status(403).json({ error: `Maximo atingido, mude para o plano pro` })
+  }
+
+  next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  const todo = user && user.todos.find(todo => todo.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: 'ID inválido' })
+  }
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: 'ID inválido' })
+  }
+
+  if (!todo) {
+    return response.status(404).json({ error: 'ID inválido' })
+  }
+
+  if (todo.id !== id) {
+    return response.status(404).json({ error: 'ID inválido' })
+  }
+
+  request.todo = todo;
+  request.user = user;
+
+  next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: 'id not fount' })
+  }
+
+  request.user = user;
+
+  next();
 }
 
 app.post('/users', (request, response) => {
